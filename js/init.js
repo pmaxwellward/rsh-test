af8eab7d3000a2f8c4f7fc88103c2a41db9c6ad6
pmaxwellward <pmaxwellward@gmail.com>
2020-12-01 13:00:48 -0500

    second

---
+++ js/init.js
@@ -0,0 +1,20 @@
+
+
+(function(exports) {
+    'use strict';
+
+    var settings = {
+        Model: YouTubeAPIModel,
+        PlayerView: YouTubePlayerView,
+        PlaylistView: PlaylistPlayerView,
+        showSearch: true,
+        skipLength: 30,
+        controlsHideTime: 3000,
+        user: "UCpaDIRGzBwp6B88WflwuFpA",
+        devKey: process.env.API_KEY,
+        showLatestChannel: false,
+        displayButtons: false
+    };
+
+    exports.app = new App(settings);
+}(window));
